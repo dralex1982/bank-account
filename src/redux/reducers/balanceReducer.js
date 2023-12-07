87672a216eaa0883e024f4295a9ac9777abe9468
Алексей Журавлев <dralex1982@gmail.com>
2023-12-08 00:51:34 +0200

    add Redux toolkit

--- src/redux/reducers/balanceReducer.js
+++ src/redux/reducers/balanceReducer.js
@@ -3,18 +3,7 @@ import {createReducer} from "@reduxjs/toolkit";
 
 const initialBalance = 10000;
 
-//Map object notation
-// const balanceReducer =
-//     createReducer(initialBalance, {
-//         [depositAction]: (state, action)=>{
-//             state.balance += action.payload;
-//         },
-//         [withdrawAction]: (state, action)=>{
-//             const res = balance - action.payload;
-//             state.balance = res < 0 ? balance : res;
-//         },
-//     });
-
+// Builder callback notation
 const balanceReducer =
     createReducer(initialBalance, builder => {
         builder
@@ -28,17 +17,5 @@ const balanceReducer =
                 return res < 0 ? balance : res;
             })
     })
-
-// function balanceReducer(balance = initialBalance, action) {
-//     switch (action.type) {
-//         case depositAction.type:
-//             return balance + action.payload;
-//         case withdrawAction.type:
-//             const res = balance - action.payload;
-//             return res < 0 ? balance : res;
-//         default:
-//             return balance;
-//     }
-// }
 
 export default balanceReducer;
