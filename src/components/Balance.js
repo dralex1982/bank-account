fb6bbf96f539fb7f6f895a71a542fbd99c666484
Алексей Журавлев <dralex1982@gmail.com>
2023-12-09 16:03:59 +0200

    add Redux UseMemo

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {useSelector} from "react-redux";
+import Bank from "./Bank";
 
 const Balance = () => {
     const balance = useSelector(state => state.account.balance);
     const quote = useSelector(state => state.quote.text);
     return (
         <div>
-            <h1 className={'text-center text-uppercase'}>Bank Hapoalim</h1>
+            <Bank info={'Hapoalim'}/>
             <h4 className={'text-center text-uppercase'}><q>{quote}</q></h4>
             <h3 className={'text-center text-uppercase'}>Balance = {balance}</h3>
         </div>
